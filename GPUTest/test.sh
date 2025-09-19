b4cb2aced293db331a6c8209eefa580497f00a1e
Zhizhou Tian <zhizhou.tian@amd.com>
2025-09-19 09:29:57 +0000

    update
    Signed-off-by: Zhizhou Tian <zhizhou.tian@amd.com>

---
+++ GPUTest/test.sh
@@ -0,0 +1,20 @@
+NCCL_P2P_LEVEL=SYS \
+NCCL_P2P_DISABLE=0 \
+VLLM_WORKER_MULTIPROC_METHOD=spawn \
+VLLM_MLA_DISABLE=1 \
+VLLM_USE_V1=1 \
+vllm serve /home/ubuntu/.cache/modelscope/hub/models/deepseek-ai/DeepSeek-R1-Distill-Llama-70B \
+       --block-size 16 \
+       --dtype float16 \
+       --enable-chunked-prefill \
+       --max-model-len 10240\
+       --gpu-memory-utilization 0.95 \
+       --tensor-parallel-size 8 \
+       --trust-remote-code \
+       --disable-log-requests \
+       --disable-log-stats \
+       --max-num-seqs 16 \
+       --max-num-batched-tokens 10240 \
+       --enable-prefix-caching \
+       --port 8888
+
