df2051ee247802efe98c2aafa05abe2cf7832d1c
Zhizhou Tian <zhizhou.tian@amd.com>
2025-09-20 12:34:13 +0000

    update 9355 data
    Signed-off-by: Zhizhou Tian <zhizhou.tian@amd.com>

---
+++ GPUTest/client.sh
@@ -0,0 +1,23 @@
+#python3 /home/ubuntu/.src/github.com/vllm/benchmarks/benchmark_serving.py \
+#!/bin/bash
+
+prompts=$1
+concurrency=$2
+
+vllm bench serve \
+	--backend vllm \
+	--model /home/ubuntu/.cache/modelscope/hub/models/deepseek-ai/DeepSeek-R1-Distill-Llama-70B \
+	--host 127.0.0.1 \
+	--port 8888 \
+	--dataset-name "sharegpt" \
+	--dataset-path "/home/ubuntu/.src/github.com/share/GPUTest/ShareGPT_V3_unfiltered_cleaned_split.json"\
+	--num-prompts $1 \
+	--sharegpt-output-len 1024 \
+	--max-concurrency $2 \
+	--burstiness 1.0 \
+	--percentile-metrics "ttft,tpot,itl" \
+	--metric-percentiles "90,99" \
+	--endpoint-type vllm 
+
+#	--served-model-name DeepSeek-R1-Distill-Llama-70B 
+#	--endpoint /v1/completions \
--- GPUTest/server.sh
+++ GPUTest/server.sh
@@ -1,20 +1,25 @@
+#!/bin/bash
+seqs=$1
+echo ${seqs}
+
 NCCL_P2P_LEVEL=SYS \
 NCCL_P2P_DISABLE=0 \
 VLLM_WORKER_MULTIPROC_METHOD=spawn \
 VLLM_MLA_DISABLE=1 \
 VLLM_USE_V1=1 \
-vllm serve /home/ubuntu/.cache/modelscope/hub/models/deepseek-ai/DeepSeek-R1-Distill-Llama-70B \
+vllm serve \
+	/home/ubuntu/.cache/modelscope/hub/models/deepseek-ai/DeepSeek-R1-Distill-Llama-70B \
        --block-size 16 \
        --dtype float16 \
        --enable-chunked-prefill \
-       --max-model-len 10240\
-       --gpu-memory-utilization 0.95 \
+       --max-model-len 4096 \
        --tensor-parallel-size 8 \
        --trust-remote-code \
        --disable-log-requests \
        --disable-log-stats \
-       --max-num-seqs 16 \
+       --max-num-seqs ${seqs} \
        --max-num-batched-tokens 10240 \
        --enable-prefix-caching \
        --port 8888
 
+    #   --gpu-memory-utilization 0.95 \
