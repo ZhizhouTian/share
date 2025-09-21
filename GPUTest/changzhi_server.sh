58eafa4eca0ddadc5949a19becf0292c7e7b02b3
Zhizhou Tian <zhizhou.tian@amd.com>
2025-09-21 08:57:16 +0000

    update
    Signed-off-by: Zhizhou Tian <zhizhou.tian@amd.com>

--- GPUTest/changzhi_server.sh
+++ GPUTest/changzhi_server.sh
@@ -1,9 +1,5 @@
-#!/bin/bash
-seqs=$1
-echo ${seqs}
-
-NCCL_P2P_LEVEL=SYS \
-NCCL_P2P_DISABLE=0 \
+#NCCL_P2P_LEVEL=SYS \
+#NCCL_P2P_DISABLE=0 \
 VLLM_WORKER_MULTIPROC_METHOD=spawn \
 VLLM_MLA_DISABLE=1 \
 VLLM_USE_V1=1 \
@@ -12,14 +8,13 @@ vllm serve \
        --block-size 16 \
        --dtype float16 \
        --enable-chunked-prefill \
-       --max-model-len 4096 \
+       --gpu-memory-utilization 0.95 \
+       --max-model-len 10240\
        --tensor-parallel-size 8 \
        --trust-remote-code \
        --disable-log-requests \
        --disable-log-stats \
-       --max-num-seqs ${seqs} \
+       --max-num-seqs 16 \
        --max-num-batched-tokens 10240 \
        --enable-prefix-caching \
        --port 8888
-
-    #   --gpu-memory-utilization 0.95 \
